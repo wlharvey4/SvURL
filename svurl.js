1b93591be1093f49b701e30345fa629718fe5aa8
wlharvey <pinecone062@gmail.com>
2019-06-08 09:12:24 -0700

    Further refactorings of sets, showing, finding

--- svurl.js
+++ svurl.js
@@ -9,55 +9,38 @@ function SvURL (sets) {
 
     this.sets = Object.entries(this.paths)
         .map(([setName, setPath]) => {
-            return { setName, setPath, set: this.getSet(setPath) }
+            return { setName, setPath, set: this.loadSet(setPath) }
         });
 
-    // this.savedSet = this.getSet(saved);
-    // this.originsSet = this.getSet(origins);
-    // this.usedSet = this.getSet(used);
-
     this.url;
-    // this.saved = saved;
-    // this.origins = origins;
-    // this.used = used;
-}
-
-
-SvURL.prototype.showSets = function () {
-    console.log(util.inspect(this.sets));
-}
-
-SvURL.prototype.showSet = function (aSet) {
-    console.log(util.inspect(this.sets[aSet]));
 }
 
 SvURL.prototype.fullPath = function () {
     return this.url ? `${this.url.origin}${this.url.pathname}` : undefined;
 }
 
-SvURL.prototype.showFullPath = function () {
-    console.log(this.fullPath());
+SvURL.prototype.showSets = function () {
+    console.log(`Show sets:
+${util.inspect(this.sets)}\n`);
+}
+
+SvURL.prototype.showSetElements = function (setName) {
+    console.log(`Show set '${setName}':
+	${util.inspect(this.sets.find(set => set.setName === setName))}\n`);
 }
 
 SvURL.prototype.showURL = function () {
     console.log(this.url);
 }
 
-// SvURL.prototype.fullPath = function() {
-//     return this.url ? `${this.url.origin}${this.url.pathname}` : undefined;
-// }
+SvURL.prototype.showURLFullPath = function () {
+    console.log(this.fullPath());
+}
 
-// SvURL.prototype.showURL = function () {
-//     console.log(this.url);
-// }
-
-// SvURL.prototype.showURLFullPath = function () {
-//     console.log(this.fullPath());
-// }
-
-SvURL.prototype.addToSet = function (aURL) {
+SvURL.prototype.addToSet = function (aSetName, aURL) {
     try {
         this.url = new URL(aURL);
+        const theSet = this.findSet(aSetName);
         this.savedSet.then(set => set.add(this.fullPath()));
         this.originsSet.then(set => set.add(this.url.origin));
     } catch (err) {
@@ -66,14 +49,26 @@ SvURL.prototype.addToSet = function (aURL) {
     }
 }
 
-SvURL.prototype.getSet = function (path) {
+SvURL.prototype.findSet = function (aSetName) { // takes a String
+    return (this.sets.find(set => set.setName === aSetName)).set; // returns a Promise
+}
+
+SvURL.prototype.showSet = function (aSet) { // takes a Promise
+    aSet.then(set => console.log(util.inspect(set)));
+}
+
+SvURL.prototype.findAndShowSet = function (aSetName) { // takes a String
+    this.showSet(this.findSet(aSetName));
+}
+
+SvURL.prototype.loadSet = function (aPath) {
     try {
-        fs.accessSync(path, fs.constants.R_OK);
+        fs.accessSync(aPath, fs.constants.R_OK);
     } catch (err) {
         if (err.code === 'ENOENT') {
             console.error(err.message);
-            fs.appendFileSync(path, '');
-            console.log(`Created ${path}`);
+            fs.appendFileSync(aPath, '');
+            console.log(`Created ${aPath}`);
         } else {
             throw err;
         }
@@ -82,7 +77,7 @@ SvURL.prototype.getSet = function (path) {
         const set = new Set();
         try {
             const rls = readline.createInterface({
-                input: fs.createReadStream(path),
+                input: fs.createReadStream(aPath),
                 removeHistoryDuplicates: true
             });
 
@@ -95,17 +90,13 @@ SvURL.prototype.getSet = function (path) {
     });
 }
 
-// SvURL.prototype.showSet = function (aSet) {
-//     aSet.then( set => console.log(util.inspect(set)) );
-// }
-
 SvURL.prototype.saveSets = function() {
     this.saveSet(this.saved, this.savedSet);
     this.saveSet(this.origins, this.originsSet);
     this.saveSet(this.used, this.usedSet);
 }
 
-SvURL.prototype.saveSet = function (aFile, aSet) {
+SvURL.prototype.saveSet = function (aPath, aSet) {
     const tmp = `${aFile}.tmp`,
           bak = `${aFile}.bak`;
     if (fs.existsSync(tmp)) fs.unlinkSync(tmp);
